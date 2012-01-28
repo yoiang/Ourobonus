677d2c7d956c68d6b177336394c9ca8ef83a6da4
Eli Delventhal <eli@ngmoco.com>
2012-01-27 22:05:42 -0800

    Code folder

---
+++ Assets/Code/Runner.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour
+{
+	public float maxSpeed = ;
+	public float jumpSpeed;
+	public float gravity;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
